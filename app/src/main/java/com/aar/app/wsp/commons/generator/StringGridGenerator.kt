4bb409428ebb8ab995c07252f2497df3a40e4267
Abdul Aris <abdularisrahmanudin10@gmail.com>
2021-01-04 06:07:07 +0700

    Convert string grid generator to kotlin

--- app/src/main/java/com/aar/app/wsp/commons/generator/StringGridGenerator.kt
+++ app/src/main/java/com/aar/app/wsp/commons/generator/StringGridGenerator.kt
@@ -1,43 +1,31 @@
-package com.aar.app.wsp.commons.generator;
+package com.aar.app.wsp.commons.generator
 
-import com.aar.app.wsp.model.Grid;
+import com.aar.app.wsp.model.Grid
 
 /**
  * Created by abdularis on 06/07/17.
  *
  * Parse dataInput kedalam array grid[][]
  */
-
-public class StringGridGenerator extends GridGenerator<String, Boolean> {
-
-    @Override
-    public Boolean setGrid(String dataInput, char[][] grid) {
-        if (dataInput == null || grid == null) return false;
-
-        dataInput = dataInput.trim();
-
-        int row = 0;
-        int col = 0;
-
-        for (int i = 0; i < dataInput.length(); i++) {
-            char c = dataInput.charAt(i);
-
-            if (c == Grid.GRID_NEWLINE_SEPARATOR) {
-                row++;
-                col = 0;
-            }
-            else {
-                if (row >= grid.length || col >= grid[0].length) {
-                    resetGrid(grid);
-                    return false;
+class StringGridGenerator : GridGenerator<String?, Boolean>() {
+    override fun setGrid(dataInput: String?, grid: Array<CharArray>): Boolean {
+        if (dataInput == null) return false
+        val trimmed = dataInput.trim { it <= ' ' }
+        var row = 0
+        var col = 0
+        for (element in trimmed) {
+            if (element == Grid.GRID_NEWLINE_SEPARATOR) {
+                row++
+                col = 0
+            } else {
+                if (row >= grid.size || col >= grid[0].size) {
+                    resetGrid(grid)
+                    return false
                 }
-
-                grid[row][col] = c;
-
-                col++;
+                grid[row][col] = element
+                col++
             }
         }
-
-        return true;
+        return true
     }
 }
